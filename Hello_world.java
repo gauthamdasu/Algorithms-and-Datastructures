cd5a3403dc5813e5f442d3117faed35303a73ecb
gauthamdasu <gauthamdasu@gmail.com>
2020-04-14 13:18:32 +0530

    First java program

---
+++ Hello_world.java
@@ -0,0 +1,14 @@
+//First sample java program to keep up with basics of java.
+//Time : 14 Apr 1.15
+
+class Hello_world{			   // The main class name and file name should be same. 
+public static void main(String argc[])
+{
+
+System.out.println("Hello world!\n"); // Print Hello world on to screen using inbuilt java class System.
+
+}
+
+
+
+}
