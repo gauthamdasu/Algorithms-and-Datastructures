313a9fbbcccc3defdf3d59e91f984f6c76990ba2
gauthamdasu <gauthamdasu@gmail.com>
2020-04-14 13:27:26 +0530

    Sample Commits to C folder

---
+++ c/first_program.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+// Simple hello world program in c and commiting to git.
+
+int main(int argc, char * argv[]){
+int x=10;
+printf("Hello world\nValue of %d*42 is %d.\n",x,x*42);
+return 0;
+}
